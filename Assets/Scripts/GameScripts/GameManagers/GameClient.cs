4228ce805c5cf8dcb4e5c00054dab416c5218737
Alp <elgunalp1@gmail.com>
2024-01-09 04:44:23 +0300

    Fix.

--- Assets/Scripts/GameScripts/GameManagers/GameClient.cs
+++ Assets/Scripts/GameScripts/GameManagers/GameClient.cs
@@ -9,16 +9,16 @@ namespace Rootcraft.CollectNumber
 
         public void SetCameraPosZoomFromGrid(int row, int column)
         {
-            if(row <= 5 && column <= 5)
-                return;
+            //if(row <= 5 && column <= 5)
+                //return;
 
             int big = math.max(row, column);
             Vector3 pos = GameCamera.transform.position; 
-            pos.z += (big - 5) * -23;
+            pos.z += (big - 5) * -35;
 
-            if(row > 5)
+            //if(row > 5)
                 pos.x += GridManager.Instance.PieceMargin.x * (row - 5) * 0.5f;
-            if(column> 5)
+            //if(column> 5)
                 pos.y += GridManager.Instance.PieceMargin.y * (column - 5) * 0.5f;
 
             GameCamera.transform.position = pos;
